9c965b2ca86b1c6847a148bf0167692653d0d03d
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2023-04-20 14:43:09 +0300

    adding notfound page

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,8 +1,42 @@
 // react
-import React from 'react'
+import React from "react";
+// react-router-dom
+import { Link } from "react-router-dom";
+
+// lottiefiles GIFs
+import { Player, Controls } from "@lottiefiles/react-lottie-player";
+import notfoundData from "../../assets/gifs/73061-search-not-found.json";
+
+// styles, icon
+import styles from "./styles.module.css";
+import { RxDashboard } from "react-icons/rx";
 
 export default function NotFound() {
+  const notFoundedGIFData = {
+    animationData: notfoundData,
+  };
+
   return (
-    <div>NotFound</div>
-  )
+    <div className={styles.notFoundPage}>
+      <div className={styles.notFoundPageCont}>
+        <Player
+          className={styles.player}
+          autoplay
+          loop
+          src={notFoundedGIFData.animationData}
+        ></Player>
+
+        <div className={styles.doAnActionCont}>
+          <Link to="/">
+            <p>
+              <span>
+                <RxDashboard className={styles.doAnActionIcon} />
+              </span>
+              <span>Dashboard</span>
+            </p>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
